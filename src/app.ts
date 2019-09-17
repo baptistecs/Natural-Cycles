0e9c1dc4f8fdd67e2e6f2ed75f7069615eeee7f6
baptistecs <clarey.sjostrand@gmail.com>
2019-09-17 22:38:18 +0200

    Session management
    add of session middleware to the app, with dev config

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import Express from 'express'
 import ControllerInterface from './interface/controller'
 import AppConfig from './interface/app-config'
+import Session, { SessionOptions } from 'express-session'
 
 class App {
   public app: Express.Application
   private appConfig: AppConfig
+  private sessionConfig: SessionOptions
 
   constructor(controllers: ControllerInterface[]) {
     const ENV = process.env.NODE_ENV || 'development' // this.app.get('env')
 
     this.appConfig = require('../config/' + ENV + '/app.json')
+    this.sessionConfig = require('../config/' + ENV + '/session.json')
 
     this.app = Express()
 
@@ -20,6 +23,7 @@ class App {
   private initializeMiddlewares() {
     this.app.use(Express.json()) // to support JSON-encoded bodies
     this.app.use(Express.urlencoded({ extended: true })) // to support URL-encoded bodies
+    this.app.use(Session(this.sessionConfig))
   }
 
   private initializeControllers(controllers: ControllerInterface[]) {
