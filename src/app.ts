b7163a5760ce9e49e5de94f29f64345ffe119851
baptistecs <clarey.sjostrand@gmail.com>
2019-09-27 15:13:37 +0200

    Request method override
    package + app implementation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import Express from 'express'
 import ControllerInterface from './interface/controller'
 import AppConfig from './interface/app-config'
 import Session, { SessionOptions } from 'express-session'
+import MethodOverride from 'method-override'
 
 class App {
   private static instance: App
@@ -33,6 +34,17 @@ class App {
   private initializeMiddlewares() {
     this.app.use(Express.json()) // to support JSON-encoded bodies
     this.app.use(Express.urlencoded({ extended: true })) // to support URL-encoded bodies
+    this.app.use(
+      // override request method (POST/GET + param => POST/GET/PUT/DELETE)
+      MethodOverride((req: Express.Request, res: Express.Response): string => {
+        if (req.body._method) {
+          var method = req.body._method
+          delete req.body._method
+          return method
+        }
+        return req.method
+      }),
+    )
     this.app.use(Session(this.sessionConfig))
   }
 
@@ -52,7 +64,7 @@ class App {
 
   public run() {
     this.app.listen(this.appConfig.port, () => {
-      console.log(`App listening on the port ${this.appConfig.port}`)
+      console.log(`App listening on the port ${this.appConfig.port}...`)
       this.runControllersOnAppStart()
     })
   }
