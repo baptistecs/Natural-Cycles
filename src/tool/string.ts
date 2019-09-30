2c75ec064c0938b63d9d269ce827438a35c06104
baptistecs <clarey.sjostrand@gmail.com>
2019-09-30 18:40:04 +0200

    String tool
    htmlEntities skip classic alphanum characters

--- src/tool/string.ts
+++ src/tool/string.ts
@@ -1,13 +1,20 @@
+// escape all non classic alpha-numeric characters
+export function htmlEntities(string: string): string {
+  return string.replace(/[^a-zA-Z0-9]/gm, function(s) {
+    // The charCodeAt() method returns an integer between 0 and 65535
+    // representing the UTF-16 code unit at the given index.
+    return '&#' + s.charCodeAt(0) + ';'
+  })
+}
+
+/* String.fromHtmlEntities = function(string) {
+  return (string+"").replace(/&#\d+;/gm,function(s) {
+      return String.fromCharCode(s.match(/\d+/gm)[0]);
+  })
+}; */
+
 /* declare global {
   interface String {
     htmlEntities(string: string): string
   }
 } */
-
-export function htmlEntities(string: string): string {
-  return string.replace(/./gm, function(s) {
-    return '&#' + s.charCodeAt(0) + ';'
-  })
-}
-
-// export default String.prototype.htmlEntities
